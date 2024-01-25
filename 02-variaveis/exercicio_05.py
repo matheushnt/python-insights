1761da41e7141115fd7434269fe7d44dac7b851c
Matheus <matheushnt06@gmail.com>
2024-01-25 17:19:19 -0300

    Exercício concluído

---
+++ 02-variaveis/exercicio_05.py
@@ -0,0 +1,7 @@
+""" Faça um Programa que converta metros para centímetros. Você pode pedir o
+comprimento em metros para o usuário (input) """
+
+cm = 100
+metros = float(input('Informe um comprimento em metros (m): '))
+conversao = cm * metros
+print(f'{metros}m possui {conversao}cm')
