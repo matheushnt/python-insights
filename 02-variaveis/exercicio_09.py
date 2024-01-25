ba2d44e3d4f4bf5d3092fe03c8f638646b33c888
Matheus <matheushnt06@gmail.com>
2024-01-25 17:44:31 -0300

    Exercício concluído

---
+++ 02-variaveis/exercicio_09.py
@@ -0,0 +1,6 @@
+""" Faça um Programa que peça a temperatura em graus Celsius,
+ transforme e mostre em graus Fahrenheit. """
+
+celsius = float(input('Informe a temperatura em graus Celsius (°C): '))
+fahrenheit = (celsius * 1.8) + 32
+print(f'{celsius}°C é igual a {fahrenheit}°F')
