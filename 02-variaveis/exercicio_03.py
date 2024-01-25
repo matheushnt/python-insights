404cb129473340c7bae9fda98df825941ab33aae
Matheus <matheushnt06@gmail.com>
2024-01-25 16:03:05 -0300

    Exercício concluído

---
+++ 02-variaveis/exercicio_03.py
@@ -0,0 +1,5 @@
+"""Faça um Programa que peça dois números e imprima a soma"""
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+soma = n1 + n2
+print(f'A soma entre {n1} e {n2} é igual a {soma}')
