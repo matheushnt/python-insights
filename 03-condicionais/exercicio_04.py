9608e34f3eaa518db8e3eab7de14fbfed088aa99
Matheus <matheushnt06@gmail.com>
2024-01-28 21:23:29 -0300

    Exercício concluído

---
+++ 03-condicionais/exercicio_04.py
@@ -0,0 +1,10 @@
+""" Faça um Programa que peça dois números e imprima o maior deles """
+
+maior = 0
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+if n1 > n2:
+    maior = n1
+else:
+    maior = n2
+print(f'O maior número informado foi {maior}')
