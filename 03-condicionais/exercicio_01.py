2cda03e6d922d5c10b6c6fdeab1cf39df3b1b941
Matheus <matheushnt06@gmail.com>
2024-01-30 14:00:07 -0300

    Exclusão do enunciado por questões de simplificação

--- 03-condicionais/exercicio_01.py
+++ 03-condicionais/exercicio_01.py
@@ -1,12 +1,3 @@
-""" Crie um programa que calcule e dê um print no bônus que os
-funcionários devem receber segundo a regra:
-
-- A meta é R$20000 em vendas
-- Se o valor de vendas for maior ou igual a meta, o valor do bônus do
-funcionário é 10% do valor de vendas
-- Caso contrário o valor de bônus do funcionário é 0
-Print o bônus dos 3 funcionários """
-
 meta = 10000
 bonus = 0
 valor_funcionario = float(input('Informe o valor de vendas: R$'))
