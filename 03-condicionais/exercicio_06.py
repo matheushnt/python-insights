8e6a39a7b2cc933e4d27ef93fa360d94339dae48
Matheus <matheushnt06@gmail.com>
2024-01-30 14:02:54 -0300

    Exclusão do enunciado por questões de simplificação

--- 03-condicionais/exercicio_06.py
+++ 03-condicionais/exercicio_06.py
@@ -1,8 +1,3 @@
-""" Faça um Programa que verifique o estado civil de uma pessoa.
-Se a letra digitada é "C" (Casado), "S" (Solteiro), "D" (Divorciado),
-"V" (Viúvo) ou "O" (outros). Conforme a letra escrita pelo usuário
-seu programa deve escrever o estado civil """
-
 estado_civil = str(input('Estado civil (Ex: C - Casado; S - Solteiro): '))
 if estado_civil == 'C':
     print('Casado')
