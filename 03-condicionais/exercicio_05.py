1b2b6ac87bc808c02d9b20eb6c702f845d93c0b1
Matheus <matheushnt06@gmail.com>
2024-01-30 14:02:34 -0300

    Exclusão do enunciado por questões de simplificação

--- 03-condicionais/exercicio_05.py
+++ 03-condicionais/exercicio_05.py
@@ -1,6 +1,3 @@
-""" Faça um Programa que peça um valor e mostre
-na tela se o valor é positivo ou negativo """
-
 num = int(input('Informe um valor: '))
 if num > 0:
     print('O valor informado é positivo')
