36b26072d58ca8378d02c02791d1686e374416f4
Matheus <matheushnt06@gmail.com>
2024-01-30 13:59:42 -0300

    Praticando o uso da estrutura match case

---
+++ 03-condicionais/exercicio_15.py
@@ -0,0 +1,17 @@
+num = int(input('Insira um valor entre 1 a 6: '))
+# Uso da Estrutura Match case
+match num:
+    case 1:
+        print('Domingo')
+    case 2:
+        print('Segunda-feira')
+    case 3:
+        print('Terça-feira')
+    case 4:
+        print('Quarta-feira')
+    case 5:
+        print('Quinta-feira')
+    case 6:
+        print('Sexta-feira')
+    case 7:
+        print('Sábado')
