83916cdca7c8c45f25652b817c3d702cc350de35
Matheus <matheushnt06@gmail.com>
2024-01-30 14:01:06 -0300

    Exclusão do enunciado por questões de simplificação

--- 03-condicionais/exercicio_03.py
+++ 03-condicionais/exercicio_03.py
@@ -1,16 +1,3 @@
-""" Crie um sistema para ser usado pelo time de controle de estoque
-de um centro de distribuição. Imagine que ao fim de todo dia, o time conta
-quantas unidades de produto existem no estoque. Se tivermos um estoque abaixo
-do estoque permitido para aquela categoria do produto, o time deve ser avisado
-(print) para fazer um novo pedido daquele produto.
-
-Cada categoria de produto tem um estoque mínimo diferente,
-segundo a regra abaixo:
-- alimentos -> Estoque mínimo: 50
-- bebidas -> Estoque mínimo: 75
-- limpeza -> Estoque mínimo: 30
-"""
-
 estoque_min_alimentos = 70
 estoque_min_bebidas = 50
 estoque_min_limpeza = 30
