eac28b5bd67c59491a54c0dbc2f14ec7a9e3b0a8
Matheus <matheushnt06@gmail.com>
2024-02-04 15:03:00 -0300

    Exercício concluído

--- 05-strings/exercicio_02.py
+++ 05-strings/exercicio_02.py
@@ -1,7 +1,7 @@
 print(' VALIDAÇÃO DE CPF '.center(40, '-'))
-cpf = str(input('Informe seu CPF: '))
+cpf = str(input('Informe seu CPF: ')).strip()
 cpf_form = cpf.replace('.', '').replace('-', '').replace(' ', '')
 if cpf_form.isnumeric() and len(cpf_form) == 11:
-    print(f'Seu CPF é: {cpf}')
+    print(f'Seu CPF é: {cpf_form}')
 else:
     print('CPF inválido')
