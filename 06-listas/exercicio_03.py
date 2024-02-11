6bbf583851b09a680ee85984dc8f82b82122bc75
Matheus <matheushnt06@gmail.com>
2024-02-11 16:51:33 -0300

    Exercício concluído

--- 06-listas/exercicio_03.py
+++ 06-listas/exercicio_03.py
@@ -1,13 +1,15 @@
 print('SISTEMA DE CADASTRO DE PRODUTOS'.center(45, '-'))
 
 # Lista de produtos vazia
-produtos = list()
+produtos = ['monitor', 'mouse', 'teclado', 'processador',
+            'mouse pad', 'gabinete', 'air cooler', 'fams']
 
 produto = str(input('Produto a ser cadastrado: ')).lower()
 
 if produto not in produtos:
     produtos.append(produto)
     print(f'Produto {produto} cadastrado com sucesso!')
-    print(f'Produtos cadastros: {produtos}')
+    print(f'Produtos cadastros:')
+    print('\n'.join(produtos))
 else:
     print(f'Produto já cadastrado. Insira outro produto.')
