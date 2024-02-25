abc075c9f473aec6e81fa2d6541c6a18f2685904
Matheus <matheushnt06@gmail.com>
2024-02-25 16:51:52 -0300

    Exercício corrigido

--- 08-while/exercicio_04.py
+++ 08-while/exercicio_04.py
@@ -1,7 +1,7 @@
 print('ANÁLISE DA POPULAÇÃO'.center(30, '='))
 
 PAIS_A = 80000
-TAXA_CRESCIMENTO_PAIS_A = 0.03
+TAXA_CRESCIMENTO_PAIS_A = 0.3
 PERCENTUAL_PAIS_A = PAIS_A * TAXA_CRESCIMENTO_PAIS_A
 
 PAIS_B = 200000
@@ -9,11 +9,12 @@ TAXA_CRESCIMENTO_PAIS_B = 0.015
 PERCENTUAL_PAIS_B = PAIS_B * TAXA_CRESCIMENTO_PAIS_B
 
 aumento_populacao_pais_a = anos = 0
-while aumento_populacao_pais_a < PAIS_B:
-    aumento_populacao_pais_a += PERCENTUAL_PAIS_A
+while PAIS_A <= PAIS_B:
+    aumento_populacao_pais_a = PERCENTUAL_PAIS_A
+    PAIS_A += aumento_populacao_pais_a
     anos += 1
 
-if aumento_populacao_pais_a >= PAIS_B:
+if PAIS_A >= PAIS_B:
     print(f'É necessário {anos} anos para o País A chegar a marca de ', end='')
-    print(f'{aumento_populacao_pais_a} habitantes. Consequentemente, ', end='')
+    print(f'{PAIS_A} habitantes. Consequentemente, ', end='')
     print('ultrapassando o País B.')
