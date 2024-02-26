6facc5849a01017c6da248ac056903c26b61be9e
Matheus <matheushnt06@gmail.com>
2024-02-26 15:40:49 -0300

    Exercício concluído

--- 08-while/exercicio_10.py
+++ 08-while/exercicio_10.py
@@ -2,7 +2,12 @@ print('BARATÃO DO MATH'.center(35, '='))
 
 # Preço de todos os produtos da loja
 PRECO = 1.99
+
+# Limitando a quantidade de produtos para 50 unidades
 qtde_itens = int(input('Quantidade de itens do cliente: '))
+while qtde_itens > 50:
+    qtde_itens = int(input('Quantidade de itens do cliente: '))
+
 c = total = 0
 
 # Estrutura para calcular o valor total a ser pago pelo cliente
