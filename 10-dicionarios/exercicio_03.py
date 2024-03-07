270969dbe14d2b65a3b064b00075b95470b0a795
Matheus <matheushnt06@gmail.com>
2024-03-07 18:31:14 -0300

    Exercício concluído

---
+++ 10-dicionarios/exercicio_03.py
@@ -0,0 +1,31 @@
+print(' MANIPULAÇÃO DE DICIONÁRIOS '.center(40, '='))
+
+produtos = [
+    'iphone', 'galaxy', 'ipad', 'tv',
+    'máquina de café', 'kindle', 'geladeira', 'adega',
+    'notebook dell', 'notebook hp', 'notebook asus',
+    'microsoft surface', 'webcam', 'caixa de som',
+    'microfone', 'câmera canon'
+]
+
+vendas2019 = [
+    558147, 712350, 573823, 405252,
+    718654, 531580, 973139, 892292,
+    422760, 154753, 887061, 438508,
+    237467, 489705, 328311, 591120]
+
+vendas2020 = [
+    951642, 244295, 26964, 787604,
+    867660, 78830, 710331, 646016,
+    694913, 539704, 324831, 667179,
+    295633, 725316, 644622, 994303
+]
+
+# Unir as duas listas em uma única lista de tuplas
+vendas = list(zip(vendas2019, vendas2020))
+# Criando o dicionário
+produtos_vendas = dict(zip(produtos, vendas))
+
+for k, v in produtos_vendas.items():
+    print(f'{k}: {v}')
+    print('-' * 35)
