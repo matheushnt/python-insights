d05de0fe8b2a8e25fa116414024bd71a11c1ead2
Matheus <matheushnt06@gmail.com>
2024-03-28 15:22:01 -0300

    Exercício concluído

--- 12-funcoes/exercicio_02.py
+++ 12-funcoes/exercicio_02.py
@@ -1,9 +1,10 @@
 def calcular_imposto(preco=0, custo=0, lucro=0):
     imposto = preco - custo - lucro
     percentual = (imposto / preco) * 100
-    print(f'O Percentual do Imposto aplicado foi de {percentual:.2f}')
+    print(f'A carga tributária foi de {percentual:.2f}%')
 
 
 print(' CÁLCULO DE CARGA TRIBUTÁRIA '.center(45, '='))
 calcular_imposto(2000, 500, 1000)
 calcular_imposto(4560, 900, 2300)
+calcular_imposto(490, 100, 230)
