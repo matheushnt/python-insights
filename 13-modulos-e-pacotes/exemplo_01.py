fd991cf56d54794aebcbac2a700969c2ab85f773
Matheus <matheushnt06@gmail.com>
2024-03-31 15:56:57 -0300

    Exemplo feito

---
+++ 13-modulos-e-pacotes/exemplo_01.py
@@ -0,0 +1,16 @@
+import math_operations
+
+resultado_soma = math_operations.somar(40, 75)
+print(resultado_soma)
+
+resultado_subtracao = math_operations.subtrair(20, 15)
+print(resultado_subtracao)
+
+resultado_multiplicacao = math_operations.multiplicar(30, 5)
+print(resultado_multiplicacao)
+
+resultado_divisao = math_operations.dividir(98, 4)
+print(resultado_divisao)
+
+resultado_fatorial = math_operations.fatorial(5)
+print(resultado_fatorial)
